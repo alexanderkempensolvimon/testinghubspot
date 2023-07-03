d7e91efdf8a2bef2271497b35e4620e1176e84bc
Alexander Kempen <alexander.kempen@solvimon.com>
2023-07-03 15:18:31 +0200

    2nd

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,11 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getCustomers();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function getCustomers() {
+  var  url = "https://api.hubapi.com/crm/v3/objects/contacts"+apiKey;
+
+$.ajax(url,{success: function(data) {
+  console.log(data);
+}})
 }
